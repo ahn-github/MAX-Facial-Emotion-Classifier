062b2da25a124a4875ca69ec7ab6ffbc424a9f52
Nick Pentreath <nickp@za.ibm.com>
2019-07-05 12:22:37 +0200

    Bump version for next release

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ RESTPLUS_MASK_SWAGGER = False
 SWAGGER_UI_DOC_EXPANSION = 'none'
 API_TITLE = 'MAX Facial Emotion Classifier'
 API_DESC = 'Detect faces in an image and predict the emotional state of each person'
-API_VERSION = '1.0.0'
+API_VERSION = '1.1.0'
 
 # default model
 MODEL_NAME = API_TITLE
